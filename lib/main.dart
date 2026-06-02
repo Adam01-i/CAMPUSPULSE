900fa5dcd2f9325e0b05d2b079db0bc18f83a3bd
Adama Seck <seckmote@gmail.com>
2026-06-02 22:58:07 +0000

    Ajout de la configuration Firebase et des dépendances associées

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,31 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
+
 import 'core/services/notification_service.dart';
 import 'features/navigation/presentation/pages/main_navigation_page.dart';
+
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   if (!Platform.isLinux) {
     await NotificationService.instance.initialize();
   }
 
-  await initializeDateFormatting('fr_FR', null);
+  await initializeDateFormatting(
+    'fr_FR',
+    null,
+  );
 
   runApp(
     const ProviderScope(
